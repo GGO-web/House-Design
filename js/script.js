dff218b707770b7056dc471d0615bc8b6d96d6d2
GGO-web <68842054+GGO-web@users.noreply.github.com>
2021-05-30 22:17:05 +0300

    Added form validate

--- js/script.js
+++ js/script.js
@@ -42,6 +42,26 @@ window.addEventListener("scroll", () => {
    }
 });
 
+new JustValidate(".js-form", {
+   rules: {
+      name: {
+         required: true,
+         minLength: 3,
+         maxLength: 15,
+      },
+      email: {
+         required: true,
+         email: true,
+      },
+   },
+   messages: {
+      name: {
+         minLength: `The field must contain a <br> minimum of 3 characters`,
+         maxLength: `The field must contain <br> up to 15 characters`,
+      },
+   },
+});
+
 fsLightboxInstances["gallery"].props.onOpen = () => {
    const header = document.querySelector(".header");
 
